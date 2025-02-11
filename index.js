8de2e1cecfe8277d8b1c24fb8d742f68085d9c13
shemil01 <shemilmk1033@gmail.com>
2025-02-11 10:11:53 +0530

    updated

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
 
-const authRoute = require('./routes/authRoute')
-const taskRoute = require('./routes/taskRoute')
-const adminRoute = require('./routes/adminRoute')
+const authRoute = require("./routes/authRoute");
+const taskRoute = require("./routes/taskRoute");
+const adminRoute = require("./routes/adminRoute");
 
-dotenv.config()
+dotenv.config();
 
+const app = express();
 
-const app = express()
-// app.use(
-//     cors({
-//       origin: "https://task-management-client-black.vercel.app/",
-//       credentials: true,
-//       methods: "GET,POST,PUT,DELETE",
-//     })
-//   );
-
-  app.use(cors({
+app.use(
+  cors({
     origin: "https://task-management-client-black.vercel.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-  }));
-  
-  app.use(express.json())
+  })
+);
+
+app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api", authRoute);
+app.use("/api", taskRoute);
+app.use("/api", adminRoute);
 
-app.use('/api',authRoute)
-app.use('/api',taskRoute)
-app.use('/api',adminRoute)
+connectDB();
 
-connectDB()
+const PORT = process.env.PORT;
 
-// app.use(errorHandler)
-const PORT = process.env.PORT
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
